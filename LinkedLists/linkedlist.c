4668609a1e05a6fcf9962d3e369ca58112268c4a
Nikolas Novo <nikolasnovo03@gmail.com>
2022-04-05 10:16:08 -0300

    Documentação das Funções

--- LinkedLists/linkedlist.c
+++ LinkedLists/linkedlist.c
@@ -4,7 +4,7 @@
 /**
 * @brief Função que aloca um espaço do tamanho de uma Celula na memória
 * @param info -> Número inteiro a ser guardado nessa Celula
-* @return novaCelula, a nova célula som os apontamentos de memória para o próximo ou anterior
+* @return novaCelula, a nova célula com os apontamentos de memória para o próximo ou anterior apontando para NULL
 */
 Celula llCriaCelula(int info) {
 
@@ -21,9 +21,9 @@ Celula llCriaCelula(int info) {
 }
 
 /**
-* @brief 
+* @brief Função que aloca um espaço do tamanho de uma Lista na memória
 * @param 
-* @return 
+* @return novaLista, a nova Lista com os apontamentos de memória para o início e fim apontando para NULL
 */
 Lista* llCriarLista() {
 
@@ -39,9 +39,9 @@ Lista* llCriarLista() {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que cria uma Celula e a insere no fim da Lista
+* @param lista -> Lista na qual a nova Celula vai ser inserida
+* @param info -> Número inteiro a ser guardado nessa nova Celula
 */
 void llInsereNoFim(Lista* lista, int info) {
 
@@ -74,9 +74,9 @@ void llInsereNoFim(Lista* lista, int info) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que cria uma Celula e a insere no início da Lista
+* @param lista -> Lista na qual a nova Celula vai ser inserida
+* @param info -> Número inteiro a ser guardado nessa nova Celula
 */
 void llInsereNoInicio(Lista* lista, int info) {
 
@@ -109,9 +109,11 @@ void llInsereNoInicio(Lista* lista, int info) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que cria uma Celula e a insere antes de um elemento da lista, 
+	caso esse elemento não exista é inserida no início
+* @param lista -> Lista na qual a nova Celula vai ser inserida
+* @param info -> Número inteiro a ser guardado nessa nova Celula
+* @param alvo -> Número o qual a nova Celula vai ser inserida antes
 */
 void llInsereAntesDe(Lista* lista, int info, int alvo) {
 
@@ -155,9 +157,11 @@ void llInsereAntesDe(Lista* lista, int info, int alvo) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que cria uma Celula e a insere depois de um elemento da lista,
+	caso esse elemento não exista é inserida no fim
+* @param lista -> Lista na qual a nova Celula vai ser inserida
+* @param info -> Número inteiro a ser guardado nessa nova Celula
+* @param alvo -> Número o qual a nova Celula vai ser inserida depois
 */
 void llInsereDepoisDe(Lista* lista, int info, int alvo) {
 
@@ -201,9 +205,9 @@ void llInsereDepoisDe(Lista* lista, int info, int alvo) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que cria uma Celula e a insere de forma ordenada na lista
+* @param lista -> Lista na qual a nova Celula vai ser inserida
+* @param info -> Número inteiro a ser guardado nessa nova Celula
 */
 void llInsereOrdenado(Lista* lista, int info) {
 
@@ -252,9 +256,8 @@ void llInsereOrdenado(Lista* lista, int info) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que remove o último elemento da lista
+* @param lista -> Lista a qual vai ter o fim removido
 */
 void llRemoveFim(Lista* lista) {
 
@@ -277,9 +280,8 @@ void llRemoveFim(Lista* lista) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que remove o primeiro elemento da lista
+* @param lista -> Lista a qual vai ter o início removido
 */
 void llRemoveInicio(Lista* lista) {
 
@@ -302,14 +304,16 @@ void llRemoveInicio(Lista* lista) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que busca um elemento na lista e se o encontrar, o remove
+* @param lista -> Lista a qual vai ter o elemento removido
+* @param alvo -> Número o qual vai ser buscado na lista para ser removido caso exista
+* @return buscada -> A Celula removida
+* @return NULL -> Caso o alvo não exista na lista
 */
-Celula llRemoveNo(Lista* lista, int info) {
+Celula llRemoveNo(Lista* lista, int alvo) {
 
 	//Busca a Celula que será removida.
-	Celula buscada = llBusca(lista, info);
+	Celula buscada = llBusca(lista, alvo);
 
 	//Caso 1:
 	//Caso essa Celula exista:
@@ -352,9 +356,11 @@ Celula llRemoveNo(Lista* lista, int info) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que verifica se um elemento existe na lista, e caso exista, o retorna
+* @param lista -> Lista na qual vai acontecer a busca
+* @param info -> Número a ser buscado na Lista
+* @return aux -> O Celula do numero buscado
+* @return NULL -> Caso o número não exista na lista
 */
 Celula llBusca(Lista* lista, int info) {
 	
@@ -374,9 +380,8 @@ Celula llBusca(Lista* lista, int info) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que passa pelos elementos da lista e os imprime
+* @param lista -> lista a qual vai ter os elementos impressos
 */
 void llImprimeLista(Lista* lista) {
 
@@ -389,6 +394,10 @@ void llImprimeLista(Lista* lista) {
 	printf("\n");
 }
 
+/**
+* @brief Função que passa pelos elementos da lista e imprime os apontamentos de memória de cada elemento
+* @param lista -> lista a qual vai ter os apontamentos impressos
+*/
 void llImprimePonteiros(Lista* lista) {
 
 	//Percorre a Lisa inteira [...]
@@ -401,9 +410,10 @@ void llImprimePonteiros(Lista* lista) {
 }
 
 /**
-* @brief
-* @param
-* @return
+* @brief Função que verifica se a lista tem um ciclo em algum ponto
+* @param lista -> a lista que vai ser verificada
+* @return 1 -> caso exista um ciclo
+* @return 0 -> caso não exista um ciclo
 */
 int llTemCiclo(Lista* lista) {
 
@@ -433,7 +443,7 @@ int llTemCiclo(Lista* lista) {
 }
 
 /**
-* @brief
+* @brief 
 * @param
 * @return
 */
@@ -453,39 +463,5 @@ Celula swapCelulas(Celula a, Celula b) {
 * @return
 */
 Celula bubbleList(Celula inicio) {
-	if (inicio == NULL || inicio->prox == NULL)
-	{
-		return;
-	}
-
-	Celula atual, prev, head;
-	int swapped;
-
-	head = llCriaCelula(-999);
-	head->prox = inicio;
-	do
-	{
-		swapped = 0;
-		prev = head;
-		atual = head->prox;
-		while (atual->prox != NULL)
-		{
-			if (atual->info > atual->prox->info) 
-			{
-				prev->prox = swapCelulas(atual, atual->prox);
-				swapped = 1;
-			}
-
-			prev = atual;
-			if (atual->prox != NULL)
-			{
-				atual = atual->prox;
-			}
-		}
-
-	} while (swapped);
-
-	atual = head->prox;
-	free(head);
-	return atual;
+	
 }
